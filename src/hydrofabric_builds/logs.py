d2307ff7d05d8b3a2df1df7e0fd31c7513d4d648
taddyb <tkb5476@psu.edu>
2025-11-18 09:34:29 -0500

    added logging file handler implementation

---
+++ src/hydrofabric_builds/logs.py
@@ -0,0 +1,29 @@
+import logging
+import logging.handlers
+import os
+
+from dotenv import load_dotenv
+from pyprojroot import here
+
+
+def setup_logging() -> logging.Logger:
+    """Configures the hf_runner logging"""
+    load_dotenv(here() / ".env")
+
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+    logger = logging.getLogger(__name__)
+
+    log_file_path = os.getenv("LOG_FILE_PATH", "hf_builder.log")
+    max_bytes = int(os.getenv("LOG_MAX_BYTES", 10485760))  # Default 10 MB
+    backup_count = int(os.getenv("LOG_BACKUP_COUNT", 5))
+
+    file_handler = logging.handlers.RotatingFileHandler(
+        log_file_path, maxBytes=max_bytes, backupCount=backup_count
+    )
+
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.DEBUG)
+    logging.getLogger().addHandler(file_handler)
+    return logger
