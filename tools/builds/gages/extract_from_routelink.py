9ea50ee199584a97613c7b8f69c2b613fd9be6f3
Brodie Alexander <baalexander2@crimson.ua.edu>
2026-02-24 14:38:07 -0600

    Removed old code

--- tools/builds/gages/extract_from_routelink.py
+++ tools/builds/gages/extract_from_routelink.py
@@ -2,7 +2,6 @@ import argparse
 from pathlib import Path
 
 import geopandas
-import pandas as pd
 
 
 def extract_from_routelink(routelink: Path, output_csv: Path, shape: Path | None) -> None:
@@ -36,44 +35,6 @@ def extract_from_routelink(routelink: Path, output_csv: Path, shape: Path | None
     gages["gageid"] = gages["gageid"].str.strip()
 
     gages[["gageid", "lat", "lon"]].to_csv(output_csv, index=False)
-    """Extract gages from RouteLink file
-
-    Use ogr2ogr to convert NC file to GPKG and add EPSG:4326 georef i.e. ogr2ogr RouteLink.gpkg RouteLink.nc -t_srs EPSG:4326 -s_srs EPSG:4326
-
-    Parameters
-    ----------
-    routelink : Path
-        RouteLink file to extract from
-    output_csv: Path
-        Path to write gage list CSV
-    shape: Path | None
-        Shapefile to use for clipping
-    """
-    gages = geopandas.read_file(routelink).to_crs(epsg=4326)
-    coords = gages.get_coordinates()
-    coords_x = coords["x"]
-    coords_y = coords["y"]
-
-    # Test for "gages" field to be non empty and, if shapefile given, test for geometry intersection
-    merged_geom = geopandas.read_file(shape).to_crs(epsg=4326)["geometry"].union_all() if shape else None
-    gage_filter = (
-        lambda idx: gages["geometry"][idx].intersects(merged_geom) and gages["gages"][idx].strip() != ""
-        if shape
-        else lambda idx: gages["gages"][idx].strip() != ""
-    )
-
-    # Collect all gages that pass filter check into a DataFrame
-    filtered_gages = pd.concat(
-        pd.DataFrame(
-            [[gages["gages"][idx].strip(), coords_x[idx], coords_y[idx]]], columns=["gageid", "lon", "lat"]
-        )
-        for idx in filter(
-            gage_filter,
-            (idx for idx, _ in gages.iterrows()),
-        )
-    )
-
-    filtered_gages.to_csv(output_csv, index=False)
 
 
 if __name__ == "__main__":
