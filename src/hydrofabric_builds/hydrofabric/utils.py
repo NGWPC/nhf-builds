b472750113043cc450fba3610086da5d79a259d6
Quercus Hamlin <75846376+quercoak@users.noreply.github.com>
2026-03-10 09:56:43 -0400

    NGWPC-9893: AK divides (#24)

--- src/hydrofabric_builds/hydrofabric/utils.py
+++ src/hydrofabric_builds/hydrofabric/utils.py
@@ -174,12 +174,37 @@ def _combine_hydrofabrics(
             final_virtual_flowpaths = final_virtual_flowpaths.to_crs(crs)
         elif final_virtual_flowpaths.crs is None:
             final_virtual_flowpaths = final_virtual_flowpaths.set_crs(crs)
+    else:
+        # Some areas (e.g. Alaska) produce no virtual flowpaths; create empty GeoDataFrame with expected schema
+        final_virtual_flowpaths = gpd.GeoDataFrame(
+            columns=[
+                "virtual_fp_id",
+                "dn_virtual_nex_id",
+                "length_km",
+                "area_sqkm",
+                "percentage_area_contribution",
+                "vpu_id",
+                "geometry",
+            ],
+            crs=crs,
+        )
 
     if final_virtual_nexus is not None:
         if final_virtual_nexus.crs is not None and final_virtual_nexus.crs != crs:
             final_virtual_nexus = final_virtual_nexus.to_crs(crs)
         elif final_virtual_nexus.crs is None:
             final_virtual_nexus = final_virtual_nexus.set_crs(crs)
+
+    else:
+        final_virtual_nexus = gpd.GeoDataFrame(
+            columns=[
+                "virtual_nex_id",
+                "dn_virtual_fp_id",
+                "vpu_id",
+                "geometry",
+            ],
+            crs=crs,
+        )
 
     # Line merge flowpaths to remove multilinestring geometries
     final_flowpaths = final_flowpaths.assign(geometry=final_flowpaths.line_merge())
