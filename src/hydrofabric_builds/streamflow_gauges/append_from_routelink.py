141bd001ac1e15ca9dcbad467321b0a902242fe0
Brodie Alexander <baalexander2@crimson.ua.edu>
2026-03-12 16:00:12 -0500

    fix column regression

--- src/hydrofabric_builds/streamflow_gauges/append_from_routelink.py
+++ src/hydrofabric_builds/streamflow_gauges/append_from_routelink.py
@@ -42,7 +42,7 @@ def append_from_routelink(
 
     gages = gpd.GeoDataFrame(gages[["geometry", "site_no"]][~gages["site_no"].isin(gdf["site_no"])].copy())
     logger.info(f"gages: added {len(gages)} gages from RouteLink not already present in dataset")
-    gages["status"] = gages["status"].fillna("routelink")
+    gages["status"] = "routelink"
     gages = pd.concat([gdf, gages])
     gages["geometry"] = gages["geometry"].force_2d()
 
