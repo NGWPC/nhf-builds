c0823c93285a2457f32dd20373049cb02614f334
Quercus Hamlin <quercus.hamlin@outlook.com>
2026-07-22 16:12:29 -0400

    feat: usace crosswalk to lakes

--- tools/builds/gages/download_usace.py
+++ tools/builds/gages/download_usace.py
@@ -1,6 +1,7 @@
 import argparse
 from pathlib import Path
 
+import geopandas as gpd
 import pandas as pd
 import requests
 from requests.exceptions import RequestException
@@ -10,7 +11,7 @@ GROUPS = "location/group?location-category-like=flow"
 LOCATION_URL = "locations/{site}?office={office}"
 
 
-def download_usace(max_retries: int = 3, output_file: Path = Path("./usace_gages.csv")) -> None:
+def download_usace(max_retries: int = 3, output_file: Path = Path("./usace_gages.csv")) -> gpd.GeoDataFrame:
     """Download USACE locations with flow type data.
 
     Parameters
@@ -78,22 +79,153 @@ def download_usace(max_retries: int = 3, output_file: Path = Path("./usace_gages
     df = pd.DataFrame.from_records(
         data=output, columns=["location", "office", "latitude", "longitude", "full-name"]
     )
+    gdf = gpd.GeoDataFrame(
+        crs=4326,
+        data={"location": df["location"], "office": df["office"], "full-name": df["full-name"]},
+        geometry=gpd.points_from_xy(x=df["longitude"], y=df["latitude"]),
+    )
 
-    df.to_csv(output_file)
     print(f"Failed gages: {len(failed)}")
+    return gdf
 
 
-def crosswalk_usace() -> None:
-    pass
+def crosswalk_usace_lakes(
+    gages: gpd.GeoDataFrame,
+    lakes_1: gpd.GeoDataFrame,
+    lakes_2: gpd.GeoDataFrame | None,
+    lakes_1_key: str,
+    lakes_2_key: str | None,
+    buffer: int = 300,
+) -> gpd.GeoDataFrame:
+    """Crosswalk gages to 1 or 2 lake polygon layers. The output will be called `lake_id`
+
+    Parameters
+    ----------
+    gages : gpd.GeoDataFrame
+        Gage gdf
+    lakes_1 : gpd.GeoDataFrame
+        The primary lake polygons to crosswalk to
+    lakes_2 : gpd.GeoDataFrame | None
+        A secondary set of lake polygons to crosswalk to
+    lakes_1_key : str
+        ID in first lake layer
+    lakes_2_key : str | None
+        ID in second lake layer
+    buffer : int, optional
+        Buffer for spatial join, by default 300
+
+    Returns
+    -------
+    gpd.GeoDataFrame
+        GDF of gages with new columns `lake_id` and `dist_to_lake`
+    """
+    # join gages to first lakes polygon layer
+    gages = gages.to_crs(lakes_1.crs)
+    gages_joined = gpd.sjoin_nearest(
+        gages, lakes_1, how="left", max_distance=buffer, distance_col="dist_to_lake"
+    )
+    gages_joined.rename(columns={lakes_1_key: "lake_id"}, inplace=True)
+    gages_joined = (
+        gages_joined[["location", "office", "full-name", "dist_to_lake", "lake_id", "geometry"]]
+        .copy()
+        .reset_index(drop=True)
+    )
+
+    # join missing lakes to optional second lakes polygons layer
+    if isinstance(lakes_2, gpd.GeoDataFrame):
+        lakes_2 = lakes_2.to_crs(lakes_1.crs)
+        # separate missing gages
+        gages_missing = gages_joined.loc[
+            gages_joined["lake_id"].isnull(), ["location", "office", "full-name", "geometry"]
+        ].copy()
+        gages_joined = gages_joined.loc[~gages_joined["lake_id"].isnull()].copy()
+
+        gages_joined_2 = gpd.sjoin_nearest(
+            gages_missing, lakes_2, how="left", max_distance=buffer, distance_col="dist_to_lake"
+        )
+        gages_joined_2.rename(columns={lakes_2_key: "lake_id"}, inplace=True)
+        gages_joined_2 = (
+            gages_joined_2[["location", "office", "full-name", "dist_to_lake", "lake_id", "geometry"]]
+            .copy()
+            .reset_index(drop=True)
+        )
+        output = pd.concat([gages_joined, gages_joined_2])
+    else:
+        output = gages_joined
+
+    return output
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--output",
+        "--download",
+        action="store_true",
+        help="Option to download USACE data",
+    )
+    parser.add_argument(
+        "--crosswalk",
+        action="store_true",
+        help="Option to crosswalk USACE data to lakes polygon layer",
+    )
+    parser.add_argument(
+        "--output-download",
         type=Path,
-        default="./usbr.gpkg",
+        default="./usace_download.gpkg",
         help="Output path for USBR data",
     )
+    parser.add_argument(
+        "--output-crosswalk",
+        type=Path,
+        default="./usace_crosswalk.gpkg",
+        help="Output path for USBR data",
+    )
+    parser.add_argument(
+        "--lakes-1",
+        type=Path,
+        default="./data/sconus/lakes/input/nwm_lakes_sconus_input.gpkg",
+        help="Input path for nwm lakes data to crosswalk. This will be the first crosswalk.",
+    )
+    parser.add_argument(
+        "--lakes-1-key",
+        type=str,
+        default="newID",
+        help="The ID key for lakes 1 layer.",
+    )
+    parser.add_argument(
+        "--lakes-2",
+        type=Path,
+        default="./data/sconus/lakes/input/reference_waterbodies.gpkg",
+        help="Input path for lakes reference waterbodies to crosswalk. This will be the second crosswalk.",
+    )
+    parser.add_argument(
+        "--lakes-2-key",
+        type=str,
+        default="comid",
+        help="The ID key for lakes 2 layer.",
+    )
     args = parser.parse_args()
-    download_usace(output_file=args.output)
+
+    usace = None
+    if args.download:
+        print("Downloading USACE data from API")
+        usace = download_usace()
+        usace.to_file(args.output_download)
+        print(f"Saved downloaded USACE data to {args.output_download}")
+
+    if args.crosswalk:
+        if not isinstance(usace, gpd.GeoDataFrame):
+            print(f"Loading USACE from {args.output_download}")
+            usace = gpd.read_file(args.output_download)
+        lakes_1 = gpd.read_file(args.lakes_1)
+        lakes_2 = gpd.read_file(args.lakes_2) if Path(args.lakes_2).exists() else None
+        print("Crosswalking USACE gages")
+        gdf = crosswalk_usace_lakes(
+            gages=usace,
+            lakes_1=lakes_1,
+            lakes_2=lakes_2,
+            lakes_1_key=args.lakes_1_key,
+            lakes_2_key=args.lakes_2_key,
+        )
+        gdf.to_file(args.output_crosswalk)
+        print(f"Saved crosswalked USACE data to {args.output_crosswalk}")
